73caa049c73b5a524e78036706aaf67336db7cc2
tomaspeirotti <tomas.peirotti@gmail.com>
2018-11-30 20:44:09 -0300

    Creacion de csv lista

---
+++ ejercicio_245_java/src/main/java/com/simulacion/clases/Empleado.java
@@ -0,0 +1,5 @@
+package com.simulacion.clases;
+
+public class Empleado {
+
+}
--- ejercicio_245_java/src/main/java/com/simulacion/clases/SimulationBusiness.java
+++ ejercicio_245_java/src/main/java/com/simulacion/clases/SimulationBusiness.java
@@ -16,6 +16,7 @@ import java.nio.file.Paths;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SimulationBusiness {
@@ -26,8 +27,7 @@ public class SimulationBusiness {
     private static final DateTimeFormatter csvDataDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
     private JSONUtils<ParametrosDTO> parametrosDTOJSONUtils;
 
-
-    public ParametrosDTO getParametrosFromJson() throws IOException {
+    public ParametrosDTO getParametrosFromJson() {
         String json = FileUtil.getFileAsString("parametros.json");
         return getParametrosDTOJSONUtils().convertJsonToObject(json, ParametrosDTO.class);
     }
@@ -43,10 +43,9 @@ public class SimulationBusiness {
         this.parametrosDTOJSONUtils = parametrosDTOJSONUtils;
     }
 
-    private List<String> getValues() {
-        List<String> values = new ArrayList<>();
-        for (int i = 0; i <= 10000; i++) values.add(String.valueOf(LocalTime.now().toNanoOfDay()));
-        return values;
+    private List<String> getcolumnasPredeterminadas() {
+        String[] columnasPredeterminadas = {"Tiempo","Frec de arrivos","Evento","RND","Prox arrivo","Emp 1 - Estado","Emp 1 - Tiempo fin At","Emp 1 - Cola","Emp 2 - Estado", "Emp 2 - Tiempo fin At", "Emp 2 - Cola"};
+        return new ArrayList<>(Arrays.asList(columnasPredeterminadas).subList(0, columnasPredeterminadas.length));
     }
 
     public void printCSV() throws IOException {
@@ -55,7 +54,8 @@ public class SimulationBusiness {
         csvFilePath = Files.createFile(csvFilePath);
         try (BufferedWriter bufferedWriter = Files.newBufferedWriter(csvFilePath);
             CSVPrinter csvPrinter = new CSVPrinter(bufferedWriter, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
-            for (int i = 0; i <= 10000; i++) csvPrinter.printRecord(getValues());
+            csvPrinter.printRecord(getcolumnasPredeterminadas());
+            for (int i = 0; i <= 10; i++) csvPrinter.printRecord(i);
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
         }
